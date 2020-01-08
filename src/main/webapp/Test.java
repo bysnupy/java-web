9c8e468c6d6bb8a79c7f8c3e7ed0bc2ad1ccf537
Daein <bysnupy@hotmail.com>
2020-01-08 18:17:16 +0900

    文字化け

---
+++ src/main/webapp/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+  public static void main(String[] args) {
+    System.out.println("Running...");
+    while (true) {
+      try {
+        Thread.sleep(3000);
+      } catch (Exception e) {
+        break;
+      }
+    }
+  }
+  public static void Test_日本語文字化けテスト() {
+  }
+}
